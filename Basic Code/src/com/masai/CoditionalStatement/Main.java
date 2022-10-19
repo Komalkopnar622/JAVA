8840237b2a844934ce129c25a4590f95191c2189
komalkopnar622 <komalkopnar10@gmail.com>
2022-10-19 20:21:39 +0530

    inital commit  done

---
+++ Basic Code/src/com/masai/CoditionalStatement/Main.java
@@ -0,0 +1,17 @@
+package com.masai.CoditionalStatement;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+			int i=10;
+			int j=10;
+			if(i==j){
+				System.out.println(i); //output is 10
+			}
+			
+			
+
+	}
+
+}
---
+++ Basic Code/src/com/masai/SwitchCase/Main.java
@@ -0,0 +1,21 @@
+package com.masai.SwitchCase;
+
+public class Main {
+public static void main(String[] args){
+	int i=10;
+	switch(i){
+	
+	case 5:System.out.println("Five");
+	         break;
+	case 10: System.out.println("Ten");
+	         break;
+	case 15:System.out.println("Fifteen");
+			break;
+	case 20: System.out.println("Twenty");
+			break;
+	default:
+			System.out.println("Default");
+		break;
+	}
+}
+}
